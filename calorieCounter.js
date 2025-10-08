038e86189b0b5ff4988e7ece4ce906bf2fec287a
ConarBailey <ceb1432@mavs.uta.edu>
2025-10-08 15:38:46 -0500

    Altered style and added Add Meal button functionality

--- calorieCounter.js
+++ calorieCounter.js
@@ -1,3 +1,11 @@
-    function template()
+    function openMealAdd()
     {
+	document.getElementById("add").style.display="flex";
+        document.getElementById("meal-input").style.display="block";
+    }
+
+    function closeMealAdd()
+    {
+        document.getElementById("add").style.display="none";
+        document.getElementById("meal-input").style.display="none";
     }
