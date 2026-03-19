f3623bcf1c40010568b7d746dc0dee6aed47c36d
Lauraluu04 <lml9874@mavs.uta.edu>
2026-03-19 16:54:43 -0500

    Update App.js

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -3,19 +3,16 @@ import Login from './Login';
 import Logout from './Logout';
 import Welcome from './Welcome';
 
+
 import Layout from './Layout';
-import Home from './Home';
-import NewPost from './NewPost';
-import PostPage from './PostPage';
 import About from './About';
 import Admin from './Admin';
 import FoodLog from './FoodLog';
-import Log from './Log';
 import Missing from './Missing';
 import Unauthorized from './Unauthorized';
 import RequireAuth from './RequireAuth';
 import {Route,Routes,useNavigate} from 'react-router-dom';
-import {useState,useEffect} from 'react';
+import {useState} from 'react';
 import {format} from 'date-fns';
 
 const ROLES = {
@@ -25,63 +22,10 @@ const ROLES = {
 }
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "My First Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 2,
-      title: "My 2nd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 3,
-      title: "My 3rd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 4,
-      title: "My Fourth Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    }
-  ])
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
-  const [postTitle, setPostTitle] = useState('');
-  const [postBody, setPostBody] = useState('');
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const filteredResults = posts.filter((post) =>
-      ((post.body).toLowerCase()).includes(search.toLowerCase())
-      || ((post.title).toLowerCase()).includes(search.toLowerCase()));
-
-    setSearchResults(filteredResults.reverse());
-  }, [posts, search])
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
-    const newPost = { id, title: postTitle, datetime, body: postBody };
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle('');
-    setPostBody('');
-    navigate('/');
-  }
-
-  const handleDelete = (id) => {
-    const postsList = posts.filter(post => post.id !== id);
-    setPosts(postsList);
-    navigate('/');
-  }
 
 return (
     <Routes>
@@ -89,20 +33,18 @@ return (
         search={search}
         setSearch={setSearch}
       />}>
-        <Route element={<Home posts={searchResults}/>}/>
         <Route index element={<Welcome/>}/>
         <Route path="welcome" element={<Welcome/>}/>
         <Route path="about" element={<About/>}/>
         <Route path="register" element={<Register/>}/>
         <Route path="login" element={<Login/>}/>
-        <Route path="home" element={<Home/>}/>
+        <Route path="unauthorized" element={<Unauthorized />} />
 
         {/* we want to protect these routes */}
         <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Welcome />} /> */}
           <Route path="foodLog" element={<FoodLog/>}/>
           <Route path="logout" element={<Logout/>}/>
-          <Route path="log" element={<Log/>}/>
         </Route>
 
         <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
