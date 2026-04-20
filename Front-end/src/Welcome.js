8984735aa02557b413820905466d44dbea3356c1
Lauraluu04 <lml9874@mavs.uta.edu>
2026-04-19 23:07:28 -0500

    Update Back-end and Front-End
    Update Back-end and Front-End

--- Front-end/src/Welcome.js
+++ Front-end/src/Welcome.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 // import { useNavigate } from 'react-router-dom';
 
 function WelcomePage() {
@@ -22,8 +24,11 @@ function WelcomePage() {
       <center><br/>
         {/* <img className='Pic1' src='../iced-coffee.png'/> */}
         <img className='Pic1' src='../logo.png'/>
-        <br/><br/><a href="login"><h3>Sign-in</h3></a><a href="register"><h3>Sign-up</h3></a><br/>
-        </center>
+        <br/><br/><Link to="/login"><h3>Sign In</h3></Link><Link to="/register"><h3>Sign Up</h3></Link><br/>
+        <a href="https://platform.fatsecret.com">
+            <img alt="Nutrition information provided by fatsecret Platform API" src="https://platform.fatsecret.com/api/static/images/powered_by_fatsecret_horizontal_brand.svg" border="0"/>
+        </a>        
+      </center>
     </main>
   );
 }
